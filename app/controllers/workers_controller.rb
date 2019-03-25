1898e8e85ac952ec455769519be4d9252b0b4181
Sebastian Poblete <sebapobletec@gmail.com>
2019-03-25 20:14:35 -0300

    Show workers and projects

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -10,6 +10,7 @@ class WorkersController < ApplicationController
   # GET /workers/1
   # GET /workers/1.json
   def show
+    @projects = Project.all
   end
 
   # GET /workers/new
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :workers
 end
+  
